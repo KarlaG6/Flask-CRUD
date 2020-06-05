6fb60c31e44297cdfd9f8668ee35d3525b4c3173
KarlaG6 <karlaisabelgt@gmail.com>
2020-06-04 23:01:13 -0500

    Build and Install, file to download on dist/

---
+++ build/lib/flaskr/__init__.py
@@ -0,0 +1,43 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path,'flaskr.sqlite'),
+    )
+    if test_config is None:
+        # Load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # Load the test config if passed in
+        app.config.from_mapping(test_config)
+    
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello Karla!'
+
+    from . import db
+    db.init_app(app)
+    # connecting db with the app
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+    # the authentication blueprint will have views 
+    # to register new users and to log in and log out
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+    
+    return app
